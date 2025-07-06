43755b7875a2476c4ed3053dc08984e4d96d3f1e
HoobaBooba31 <maksimbodrov10@gmail.com>
2025-07-06 18:52:15 +0300

    new commit

--- prac3/prac3.py
+++ prac3/prac3.py
@@ -15,7 +15,7 @@ def f(x1, x2):
 z = f(x1, x2)
 
 x1_slice = np.linspace(0, pi, 300)
-x2_fixed = cord[1]
+x2_fixed = cord[1]  
 z_slice1 = f(x1_slice, x2_fixed)
 
 x2_slice = np.linspace(0, pi, 300)
@@ -26,15 +26,20 @@ fig = plt.figure(figsize=(14, 10))
 
 ax1 = fig.add_subplot(2, 2, 1, projection='3d')
 surf1 = ax1.plot_surface(x1, x2, z, cmap='viridis', edgecolor='none')
+ax1.scatter(cord[0], cord[1], f(cord[0], cord[1]), color='red', s=50, label='Тестовая точка')
+ax1.text(cord[0], cord[1], f(cord[0], cord[1]), f'({cord[0]}, {cord[1]}, {f(cord[0], cord[1])})', color='black')
 ax1.set_title('1. Поверхность функции f(x1, x2)')
 ax1.set_xlabel('x1')
 ax1.set_ylabel('x2')
 ax1.set_zlabel('f(x1, x2)')
 fig.colorbar(surf1, ax=ax1, shrink=0.5, aspect=10)
+ax1.legend()
 ax1.view_init(elev=30, azim=45)
 
 ax2 = fig.add_subplot(2, 2, 2, projection='3d')
 surf2 = ax2.plot_surface(x1, x2, z, cmap='viridis', edgecolor='none')
+ax2.scatter(cord[0], cord[1], f(cord[0], cord[1]), color='red', s=50)
+ax2.text(cord[0], cord[1], f(cord[0], cord[1]), f'({cord[0]}, {cord[1]}, {f(cord[0], cord[1])})', color='black')
 ax2.set_title('2. Вид сверху')
 ax2.set_xlabel('x1')
 ax2.set_ylabel('x2')
