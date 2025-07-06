0b9fcaa86182b5d01501010ceb9466dceb6bedda
HoobaBooba31 <maksimbodrov10@gmail.com>
2025-07-06 16:50:38 +0300

    new commit

--- prac3/prac3.py
+++ prac3/prac3.py
@@ -1,72 +1,58 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from math import sin, pi
+import numpy as np
 
-x1 = 0
+
 cord = (2.20, 1.57)
+x = np.linspace(0, pi, 300)
+x1, x2 = np.meshgrid(x, x)
 
-data1 = [[],[],[]]
-data2 = [[],[],[]]
-data3 = [[],[],[]]
 
-while x1 <= pi:
-    x2 = 0
-    while x2 <= pi:
-        y = -(sin(x1)*pow(sin(x1**2/pi), 20)) - sin(x2)*pow(sin(2*x2**2/pi), 20)
-        data1[0].append(x1)
-        data1[1].append(x2)
-        data1[2].append(y)
-        x2 += 0.01
-    x1 += 0.01
+def f(x1, x2):
+    return -(np.sin(x1) * np.sin(x1**2 / pi)**20 + np.sin(x2) * np.sin(2 * x2**2 / pi)**20)
 
-x1 = 0
-x2 = 0
-while x1 <= pi:
-    y = -(sin(x1)*pow(sin(x1**2/pi), 20)) - sin(cord[1])*pow(sin(2*cord[1]**2/pi), 20)
-    data2[0].append(x1)
-    data2[1].append(x2)
-    data2[2].append(y)
-    x1 += 0.01
+z = f(x1, x2)
 
-x1 = 0
-x2 = 0
-while x2 <= pi:
-    y = -(sin(cord[0])*pow(sin(cord[0]**2/pi), 20)) - sin(x2)*pow(sin(2*x2**2/pi), 20)
-    data3[0].append(x1)
-    data3[1].append(x2)
-    data3[2].append(y)
-    x2 += 0.01
-    
+x1_slice = np.linspace(0, pi, 300)
+x2_fixed = cord[1]
+z_slice1 = f(x1_slice, x2_fixed)
 
-fig = plt.figure(figsize=(12, 10))
+x2_slice = np.linspace(0, pi, 300)
+x1_fixed = cord[0]
+z_slice2 = f(x1_fixed, x2_slice)
+
+fig = plt.figure(figsize=(14, 10))
 
 ax1 = fig.add_subplot(2, 2, 1, projection='3d')
-ax1.scatter(data1[0], data1[1], data1[2], c='red', marker='^')
-ax1.view_init(elev=30, azim=45)
+surf1 = ax1.plot_surface(x1, x2, z, cmap='viridis', edgecolor='none')
+ax1.set_title('1. Поверхность функции f(x1, x2)')
 ax1.set_xlabel('x1')
 ax1.set_ylabel('x2')
-ax1.set_zlabel('f(x1,x2)')
-ax1.set_title('1. Изометрия')
+ax1.set_zlabel('f(x1, x2)')
+fig.colorbar(surf1, ax=ax1, shrink=0.5, aspect=10)
+ax1.view_init(elev=30, azim=45)
 
 ax2 = fig.add_subplot(2, 2, 2, projection='3d')
-ax2.scatter(data1[0], data1[1], data1[2], c='blue', marker='^')
-ax2.view_init(elev=90, azim=90)
+surf2 = ax2.plot_surface(x1, x2, z, cmap='viridis', edgecolor='none')
+ax2.set_title('2. Вид сверху')
 ax2.set_xlabel('x1')
 ax2.set_ylabel('x2')
-ax2.set_zlabel('f(x1,x2)')
-ax2.set_title('2. Изометрия(вид сверху)')
+ax2.set_zlabel('f(x1, x2)')
+fig.colorbar(surf2, ax=ax2, shrink=0.5, aspect=10)
+ax2.view_init(elev=90, azim=-90)
 
 ax3 = fig.add_subplot(2, 2, 3)
-ax3.plot(data2[0], data2[2])
+ax3.plot(x1_slice, z_slice1, color='darkorange')
+ax3.set_title(f'3. f(x1) при x2 = {cord[1]}')
 ax3.set_xlabel('x1')
 ax3.set_ylabel('f(x1)')
-ax3.set_title(f'3. y = f(x1) при x2 = {cord[1]}')
 
 ax4 = fig.add_subplot(2, 2, 4)
-ax4.plot(data3[0], data3[2])
+ax4.plot(x2_slice, z_slice2, color='darkgreen')
+ax4.set_title(f'4. f(x2) при x1 = {cord[0]}')
 ax4.set_xlabel('x2')
 ax4.set_ylabel('f(x2)')
-ax4.set_title(f'4. y = f(x2) при x1 = {cord[0]}')
 
 plt.tight_layout()
 plt.show()
